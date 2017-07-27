24d08d89375fb896b7b984f07bceece71094989e
shadom83 <30494830+shadom83@users.noreply.github.com>
2017-07-27 17:45:35 +0800

    Create app.js

---
+++ app.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+ 
+var app = express();
+var port = process.env.PORT || 1337;
+ 
+// body parser middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+ 
+// test route
+app.get('/', function (req, res) { res.status(200).send('Hello world!'); });
+ 
+app.listen(port, function () {
+  console.log('Listening on port ' + port);
+});
